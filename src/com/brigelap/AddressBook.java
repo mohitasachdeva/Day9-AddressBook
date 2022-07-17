5e214a4e77badc5e778b14d10709296a40b3a469
HP <sachdevamohita5@gmail.com>
2022-07-17 20:39:52 +0530

    add contact detil

--- src/com/brigelap/AddressBook.java
+++ src/com/brigelap/AddressBook.java
@@ -1,8 +1,32 @@
 package com.brigelap;
-
+import java.io.*;
+import java.util.Scanner;
 public class AddressBook {
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println( " Welcome to the Address Book Programme");
-        
+        System.out.println(" Welcome to the Address Book Programme");
+        Contacts contact = addContact();
+        System.out.println(contact);
+        //Contacts editedContact = editContact(contact);
+        //System.out.println(editedContact);
     }
-}
+
+    private static Contacts addContact() {
+        Contacts contact = new Contacts();
+        System.out.println("Enter first name:");
+        contact.setFirstName(scanner.next());
+        System.out.println("Enter last name:");
+        contact.setLastName(scanner.next());
+        System.out.println("Enter address:");
+        contact.setAddress(scanner.next());
+        return contact;
+    }
+
+   // private static Contacts editContact(Contacts contact) {
+     //   System.out.println("Edit first name");
+       // contact.setFirstName(scanner.next());
+        //return contact;
+    }
+
+
